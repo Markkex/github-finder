777a106c7cc997d2c3cc23038bffe9eda785ef2f
Marco Ávila <avila.marco.dev@gmail.com>
2022-01-03 17:37:25 +0000

    env files, get all users, reducers

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import UserResults from "../components/users/UserResults";
+const Home = () => {
+  return (
+    <>
+      {/*Search Component*/}
+      <UserResults />
+    </>
+  );
+};
+
+export default Home;
