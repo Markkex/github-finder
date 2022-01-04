db95d512a7bbf74ed5eed5f61abf93c34e83a69b
Marco Ávila <avila.marco.dev@gmail.com>
2022-01-04 18:09:03 +0000

    complete application

--- src/context/Github/GithubContext.js
+++ src/context/Github/GithubContext.js
@@ -4,31 +4,67 @@ import githubReducer from "./GithubReducer";
 const GithubContext = createContext();
 
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
+    user: {},
+    repos: [],
     loading: false,
   };
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  //Get initial users (testing purposes)
-  const fetchUsers = async () => {
+  const searchUsers = async (text) => {
     setLoading();
-    const response = await fetch(`${GITHUB_URL}/users`, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
-      },
-    });
+    const params = new URLSearchParams({ q: text });
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`);
+    const { items } = await response.json();
+
+    dispatch({ type: "GET_USERS", payload: items });
+  };
+
+  const clearUsers = () => {
+    dispatch({ type: "CLEAR_USERS" });
+  };
+  //Get a single user
+  const getUser = async (login) => {
+    setLoading();
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}`);
+
+    if (response.status === 404) {
+      window.location = "/notfound";
+    } else {
+      const data = await response.json();
+
+      dispatch({ type: "GET_USER", payload: data });
+    }
+  };
+  //get user repos
+  const getUserRepos = async (login) => {
+    setLoading();
+    const params = new URLSearchParams({ sort: "created", per_page: 10 });
+    const response = await fetch(
+      `${GITHUB_URL}/users/${login}/repos?${params}`
+    );
     const data = await response.json();
 
-    dispatch({ type: "GET_USERS", payload: data });
+    dispatch({ type: "GET_REPOS", payload: data });
   };
+
   const setLoading = () => dispatch({ type: "SET_LOADING" });
   return (
     <GithubContext.Provider
-      value={{ users: state.users, loading: state.loading, fetchUsers }}
+      value={{
+        users: state.users,
+        loading: state.loading,
+        user: state.user,
+        repos: state.repos,
+        searchUsers,
+        clearUsers,
+        getUser,
+        getUserRepos,
+      }}
     >
       {children}
     </GithubContext.Provider>
